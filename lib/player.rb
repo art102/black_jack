93b1bfcc57edf68c90d1ba061a20b1babcf4f3a3
ArturDaukaev <daukaevar@bashneft.ru>
2018-07-06 11:16:36 +0500

    Add some method to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -4,4 +4,33 @@ class Player
 
   attr_accessor :name, :cards
   attr_reader :bank
+
+  def initialize(name, bank = 0)
+    @name = name
+    @cards = []
+    @bank = bank
+  end
+
+  def add_card(card)
+    cards << card
+  end
+
+  def deposit(amount)
+    self.bank += amount
+  end
+
+  def withdraw(amount)
+    self.bank -= amount
+  end
+
+  def zero_cards
+    self.cards = []
+  end
+
+  def score
+    score = 0
+    cards.each { |card| score += score > 10 ? card.ace_cost : card.cost}
+
+    score
+  end
 end
