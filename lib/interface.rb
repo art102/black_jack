64b416d2e2eb81baa25561120aa735b0dba725a6
Art102 <mckorven@gmail.com>
2018-07-07 20:09:24 +0500

    Add some methods to inteface class

--- lib/interface.rb
+++ lib/interface.rb
@@ -15,4 +15,34 @@ class Interface
     puts "Cards: #{cards_list}"
     puts "____________________\n"
   end
+
+  def print_game_status(game)
+    cls
+    view_player_info(game.player)
+    view_player_info(game.dealer, game.in_progress?)
+
+    puts ''
+    puts RESULT_MESSAGE.fetch(game.result) if game.finished?
+  end
+
+  def get_player_choice
+    puts "Ваши действия:\n"
+    PLAYER_ACTIONS.each.with_index(1) { |action, num| puts "#{num}. #{action}" }
+    gets.to_i - 1
+  end
+
+  def next_game_request
+    if game.players_bank_zero?
+      puts 'Не достаточно денег для продолжения игры.'
+      return
+    end
+
+    puts 'Хотите сыграть еще раз? (1 - да, 0 - нет)'
+    return if gets.to_i.zero?
+    game.start_game
+  end
+
+  def cls
+    system "clear"
+  end
 end
