19ce50e4d835dda9f5d00e86c39bc1e28a233d84
Art102 <mckorven@gmail.com>
2018-07-07 22:53:36 +0500

    Add Deck class

---
+++ lib/deck.rb
@@ -0,0 +1,10 @@
+class Deck
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].freeze
+  SUITS = %w(♦ ♠ ♥ ♣).freeze
+
+  attr_reader :cards
+
+  def initialize
+    @cards = build_deck
+  end
+end
