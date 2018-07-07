deee4d920c3d12a244ded71b4a25d2c405125562
Art102 <mckorven@gmail.com>
2018-07-07 22:38:35 +0500

    Include class card in main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require_relative 'lib/card'
 require_relative 'lib/interface'
 require_relative 'lib/player'
 require_relative 'lib/game'
@@ -10,3 +11,17 @@ player_name = 'Аноним' if player_name.to_s.empty?
 player = Player.new(player_name)
 
 game = Game.new(player)
+
+interface = Interface.new(game)
+
+while game.in_progress?
+  interface.print_game_status(game)
+  if game.player_turn
+    game.next_player_turn(interface.get_player_choice)
+  else
+    game.next_dealer_turn
+  end
+  interface.print_game_status(game)
+
+  interface.next_game_request if game.finished?
+end
