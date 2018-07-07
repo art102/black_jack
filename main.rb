c713e6693bc572205889c8da8d8ada1ed0c833d7
Art102 <mckorven@gmail.com>
2018-07-07 17:51:11 +0500

    Include classes to main.rb

--- main.rb
+++ main.rb
@@ -1,4 +1,11 @@
+require_relative 'lib/player'
+require_relative 'lib/game'
+
 puts 'Игра "Black Jack"'
 print "Для начала игры введите ваше имя: "
 player_name = gets.chomp
 player_name = 'Аноним' if player_name.to_s.empty?
+
+player = Player.new(player_name)
+
+game = Game.new(player)
