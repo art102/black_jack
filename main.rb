8a957db901a23b0ee9bc37a95c8f2b04751a08c4
Art102 <mckorven@gmail.com>
2018-07-08 19:55:29 +0500

    Translate message in main.rb and add strip instead chomp

--- main.rb
+++ main.rb
@@ -4,10 +4,10 @@ require_relative 'lib/interface'
 require_relative 'lib/player'
 require_relative 'lib/game'
 
-puts 'Игра "Black Jack"'
-print "Для начала игры введите ваше имя: "
-player_name = gets.chomp
-player_name = 'Аноним' if player_name.to_s.empty?
+puts 'Game "Black Jack"'
+print "Please enter your name: "
+player_name = gets.strip.capitalize
+player_name = 'Anonymous' if player_name.to_s.empty?
 
 player = Player.new(player_name)
 
